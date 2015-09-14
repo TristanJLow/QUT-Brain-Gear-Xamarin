d81d6f56b1b9b12f64b68d7a48109470394f822f
cthorne <christhorn3@gmail.com>
2015-09-14 20:33:23 +1000

    Additional work to begin MVVM initialisation.

---
+++ QUTBraingear/Data/Interfaces/IPageLifeCycleEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data
+{
+	public interface IPageLifeCycleEvents
+	{
+			void OnAppearing();
+			void OnDisappearing();
+			void OnLayoutChanged();
+
+	}
+}
+
--- QUTBraingear/QUTBraingear/Views/MyPage.xaml.cs
+++ QUTBraingear/QUTBraingear/Views/MyPage.xaml.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Data.ViewModel;
 using Xamarin.Forms;
 
 namespace QUTBraingear
 {
-	public partial class MyPage : ContentPage
+	public partial class MyPage : BaseView
 	{
 		public MyPage ()
 		{
 			InitializeComponent ();
+			/*base.Init ();
+			BindingContext = App.Locator.MyPage;*/
+
 			var upcomingQA = new List<QA> ();
 			var firstQA = new QA ("Xamarin Dev", DateTime.Now.ToString ());
 			var secondQa = new QA ("Not Xamarin", DateTime.Now.ToString ());
