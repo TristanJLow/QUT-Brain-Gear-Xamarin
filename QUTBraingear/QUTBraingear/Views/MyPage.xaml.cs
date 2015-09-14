322199341bd92f3e774f043773c682ee407c546e
cthorne <christhorn3@gmail.com>
2015-09-14 15:59:54 +1000

    Setup for basic areas of main screen UI. WIP

--- QUTBraingear/QUTBraingear/Views/MyPage.xaml.cs
+++ QUTBraingear/QUTBraingear/Views/MyPage.xaml.cs
@@ -10,12 +10,19 @@ namespace QUTBraingear
 		public MyPage ()
 		{
 			InitializeComponent ();
-			var noteList = new List<QA> ();
+			var upcomingQA = new List<QA> ();
 			var firstQA = new QA ("Xamarin Dev", DateTime.Now.ToString ());
 			var secondQa = new QA ("Not Xamarin", DateTime.Now.ToString ());
-			noteList.Add(firstQA);
-			noteList.Add(secondQa);
-			listview_QAUpcoming.ItemsSource = noteList;
+			upcomingQA.Add(firstQA);
+			upcomingQA.Add(secondQa);
+			listview_QAUpcoming.ItemsSource = upcomingQA;
+
+			var skillList = new List<Skills> ();
+			var firstSkill = new Skills ("Xamarin", "10");
+			var secondSkill = new Skills ("Design", "-10");
+			skillList.Add (firstSkill);
+			skillList.Add (secondSkill);
+			listview_pointProgress.ItemsSource = skillList;
 		}
 	}
 }
