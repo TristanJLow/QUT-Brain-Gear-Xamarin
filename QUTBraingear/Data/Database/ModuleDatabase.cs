ebb3ac6cb793a0f99d059df77840b00c605ba71a
Raguletto <emmanuel@cyberize.com.au>
2015-09-29 14:45:44 +1000

    Added Database component to module view.

---
+++ QUTBraingear/Data/Database/ModuleDatabase.cs
@@ -0,0 +1,35 @@
+﻿using SQLite.Net;
+using Xamarin.Forms;
+using System.Linq;
+using System.Collections.Generic;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace QUTBraingear.Data
+{
+	public class ModuleDatabase
+	{
+		SQLiteConnection database;
+
+		public ModuleDatabase ()
+		{
+			database = DependencyService.Get<ISqlite> ().GetConnection ();
+			if (database.TableMappings.All(t => t.MappedType.Name != typeof(Module).Name)) {
+				database.CreateTable<Module> ();
+				database.Commit ();
+			}
+		}
+
+		public List<Module> GetAll(){
+			var items = database.Table<Module> ().ToList<Module>();
+
+			return items;
+		}
+
+
+		public int InsertOrUpdateSkill(Module modules){
+			return database.Table<Module> ().Where (x => x.moduleID == modules.moduleID).Any() 
+				? database.Update (modules) : database.Insert (modules);
+		}
+	}
+}
+
--- QUTBraingear/Data/Models/Module.cs
+++ QUTBraingear/Data/Models/Module.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using SQLite.Net.Attributes;
 using QUTBraingear.Data;
 
 namespace QUTBraingear.Data
 {
 	public class Module {
-		private int moduleID;
+		[PrimaryKey, AutoIncrement]
+		public int moduleID { get; set; }
 		private string videoURL;
 		private ObservableCollection<Skills> moduleSkills = new ObservableCollection<Skills>();
 		public ObservableCollection<Comment> moduleComments { get; private set; }
@@ -16,7 +18,6 @@ namespace QUTBraingear.Data
 		}
 
 		public Module (int moduleID) {
-			moduleID = this.moduleID;
 			videoURL = "https://www.youtube.com/embed/bFdP3_TF7Ks";
 			moduleSkills.Add(new Skills ("Programming", "2"));
 			moduleSkills.Add(new Skills("C#","1"));
