b9204604b4bb23896a135ab230269dec66ee9ae0
cthorne <christhorn3@gmail.com>
2015-11-05 16:16:30 +1000

    link to module page

--- QUTBraingear/QUTBraingear/Views/ProgressMapPage.xaml.cs
+++ QUTBraingear/QUTBraingear/Views/ProgressMapPage.xaml.cs
@@ -17,7 +17,7 @@ namespace QUTBraingear
 
 		void OnModuleTap(object sender, EventArgs args) {
 			
-			((MasterDetailPage)Parent).Detail = new ModulePage ("0");
+			((MasterDetailPage)Parent).Detail = new ModulePage ("B8EA42C7-E589-4AFB-A208-9EC85D64DF38");
 		}
 
 	}
