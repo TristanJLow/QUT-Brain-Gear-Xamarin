c711a748cb7abb96ce80c4a30a059b874180a0d9
cthorne <christhorn3@gmail.com>
2015-11-05 19:00:32 +1000

    Progress page updated. Links to modules, changed to all programming.

--- QUTBraingear/QUTBraingear/Views/ProgressMapPage.xaml.cs
+++ QUTBraingear/QUTBraingear/Views/ProgressMapPage.xaml.cs
@@ -15,9 +15,19 @@ namespace QUTBraingear
 			BindingContext = App.Locator.progressMap;
 		}
 
-		void OnModuleTap(object sender, EventArgs args) {
+		void introToXamTap(object sender, EventArgs args) {
 			
 			((MasterDetailPage)Parent).Detail = new ModulePage ("B8EA42C7-E589-4AFB-A208-9EC85D64DF38");
+		}
+
+		void cSharp101Tap(object sender, EventArgs args) {
+
+			((MasterDetailPage)Parent).Detail = new ModulePage ("0C8788AE-F61D-4216-9C1A-5189B9887715");
+		}
+
+		void agileTap(object sender, EventArgs args) {
+
+			((MasterDetailPage)Parent).Detail = new ModulePage ("E08013D4-1411-4865-A1EA-BD864EFFB35E");
 		}
 
 	}
